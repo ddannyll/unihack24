8dd59133b0f5291678fdee8bd795fec62e2b331a
Jack <raoj626@gmail.com>
2024-03-02 16:42:37 +1100

    matchmaking done

--- backend/src/testmatchmaking.js
+++ backend/src/testmatchmaking.js
@@ -1,7 +1,7 @@
 import { prismaClient } from "./prisma.ts";
 import {
     calculateDistance, matchmakingStartSearch,
-  matchmakingStopsearch, pairMatchmake,multiMatchmake
+  matchmakingStopsearch, pairMatchmake,multiMatchmake, matchmakingLoop
 } from "./matchmaking.ts";
 
 
@@ -46,34 +46,45 @@ async function addUser(userId, email, gender, lat, long){
 }
  
 
-// for(let i = 16; i<30; i++){
+// for(let i = 31; i<100; i++){
 //   await addUser(`user${i}`, `user${i}@gmail.com`, "male",randfloat(-33.91943486405203,-33.91712331910307), randfloat( 151.22663446618833, 151.24108375017158))
 // }
 
+ 
 
 let allRecords = await prismaClient.user.findMany()
 let preferences = ["bball", "poker", "gaming", "drinking", "partying", "tennis", "boxing", "studying", "dancing", "knitting", "sewing", "drawing"]
 let genders = ["male", "female", "both"]
 
 
-// for (let rec of allRecords){
-//     console.log(`Adding ${rec['userId']} to queue`)
+async function startMatchmaking() {
+  while (true) {
+    let numActivities = randint(1, 3);
+    let activities = [];
 
-//     let numActivities = randint(1,3) 
-//     let activities = []
-   
-//     for(let i  =0; i<numActivities; i++){
-//         activities.push(preferences[randint(0,preferences.length-1)])
-//     } 
-//     await matchmakingStartSearch(rec['userId'], activities, "both", 300, 2)
-// }
+    for (let i = 0; i < numActivities; i++) {
+      activities.push(preferences[randint(0, preferences.length - 1)]);
+    }
 
+    let rec = allRecords[randint(0, allRecords.length - 1)];
+    console.log(`Started matchmaking search for ${rec['userId']} for ${activities}`)
+    await matchmakingStartSearch(rec['userId'], activities, "both", 1500, randint(2,6));
+
+    // Wait for 1 second before sending the next request
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+}
+
+
+// Start both loops
+matchmakingLoop();
+startMatchmaking();
 
 // let res = await pairMatchmake();
 // console.log(`Pair matchmaking:`)
 // console.log(res)
-
-console.log(await multiMatchmake())
+ 
+// await multiMatchmake()
 
 
 //node --loader ts-node/esm testmatchmaking.js
