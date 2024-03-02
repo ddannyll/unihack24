d32a7e4a6233ee2503c1cddb85f175998e565678
KimCCode <120701687+KimCCode@users.noreply.github.com>
2024-03-02 17:04:08 +1100

    Finished PUT /notification route and notification helper function

---
+++ backend/src/helpers/notificationHelper.ts
@@ -0,0 +1,40 @@
+import { Tag } from "@prisma/client";
+import { prismaClient } from "../prisma.js"
+import {Expo, ExpoPushMessage} from "expo-server-sdk";
+
+// Send a notification to every user
+export const notification = async (tag: Tag, userIds: string[]) => {
+  const expo = new Expo();
+  const messages = [];
+  for (const userId of userIds) {
+    try {
+      const user = await prismaClient.user.findFirstOrThrow({
+          where: { 
+            userId: userId,
+            NOT: {
+              notificationToken: ""
+            }
+          }
+      });
+      if (!Expo.isExpoPushToken(user.notificationToken)) {
+        throw new Error(`Push token ${user.notificationToken} is not a valid Expo push token`);
+      }
+      messages.push(user.notificationToken)
+    } catch { 
+      console.log('User not found');
+    }
+  }
+  
+  const send: ExpoPushMessage = {
+    to: messages,
+    sound: 'default',
+    body: 'This is a test notification',
+    data: { withSome: 'data' },
+  }
+
+  try {
+    await expo.sendPushNotificationsAsync([send]);
+  } catch {
+    console.log('error');
+  }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { prismaClient } from "./prisma.js";
 import usersRoutes from "./api/user.js";
-
+import notificationRoutes from "./api/notification.js";
 // import startMatchMaking from "./matchmaking.js";
 
 dotenv.config();
@@ -11,14 +11,13 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send('Hello');
 });
 
 app.use("/user", usersRoutes);
-
+app.use("/notification", notificationRoutes)
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
-
   // startMatchmaking().catch((error) => {
   //   console.error(
   //     "Matchmaking process encountered an unrecoverable error:",
