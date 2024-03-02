0e6810c62d68240dfff82ee158f98db22eaf7820
jack <raoj626@gmail.com>
2024-03-02 11:11:43 +1100

    3433fffff

--- backend/src/testmatchmaking.js
+++ backend/src/testmatchmaking.js
@@ -1,9 +1,29 @@
 import { prismaClient } from "./prisma.ts";
 import {
-  matchmakingStartSearch,
+    calculateDistance, matchmakingStartSearch,
   matchmakingStopsearch, pairMatchmake
 } from "./matchmaking.ts";
 
+
+function randint(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+/**
+ * Generates a random floating-point number between two given floating-point numbers.
+ * @param {number} min - The minimum value, inclusive.
+ * @param {number} max - The maximum value, inclusive.
+ * @returns {number} A random floating-point number between min and max.
+ */
+function randfloat(min, max )  {
+    return Math.random() * (max - min) + min;
+  }
+  
+//   unsw campus bottom left -33.91943486405203, 151.22663446618833
+//   unsw campus top right -33.91712331910307, 151.24108375017158
 
 async function addUser(userId, email, gender, lat, long){
     await prismaClient.user.create({
@@ -22,21 +42,27 @@ async function addUser(userId, email, gender, lat, long){
  
 let allRecords = await prismaClient.user.findMany()
 
+let preferences=  
+    ["bball", "poker", "gaming", "drinking",
+"partying", "tennis", "boxing", "studying", "dancing", "knitting", "sewing", "drawing"]
+ 
+
+let genders = ["male", "female", "both"]
+
 for (let rec of allRecords){
-    matchmakingStartSearch(rec['userId'], ["bball", "poker", "gaming"], "both", 0,0)
+    console.log(`Adding ${rec['userId']} to queue`)
+
+    let numActivities = randint(1,3) 
+    let activities = []
+   
+    for(let i  =0; i<numActivities; i++){
+        activities.push(preferences[randint(0,preferences.length-1)])
+    } 
+    await matchmakingStartSearch(rec['userId'], activities, "both", 2003,2)
 }
 
+console.log(await pairMatchmake())
 
-
-let allMM = await prismaClient.mMQueueElement.findMany()
-
-for (let rec of allMM){
-    console.log(rec)
-}
-
-console.log(pairMatchmake())
-
-
-
+ 
 
 //node --loader ts-node/esm testmatchmaking.js
